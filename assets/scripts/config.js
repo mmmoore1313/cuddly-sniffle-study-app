bd473a8211b727c3b1da718c9f5b487a5b45ff50
Matt Moore <Matt.M.Moore13@outlook.com>
2021-05-22 14:26:16 -0400

    Modified the production apiUrl

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://studythis-mm.herokuapp.com',
+  production: 'https://studythis-mm.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 
